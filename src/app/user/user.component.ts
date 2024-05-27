b5184eb862766dfbadd855ba466fda66c1570e69
Yash Bansal <Yash.Bansal@wissen.com>
2024-05-27 11:38:43 +0530

    Create list of user, select user dynamically and display their names

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,26 @@
-import { Component, Input, computed, signal } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  computed,
+  input,
+  output,
+  signal,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
@@ -10,22 +31,28 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
   constructor() {
     //this.avatar = DUMMY_USERS[0].avatar;
   }
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  // imagePath = computed(
-  //   () => '../../assets/users/' + this.selectedUser().avatar
-  // );
+  // imagePath = computed(() => '../../assets/users/' + this.avatar());
   get imagePath() {
-    return '../../assets/users/' + this.avatar;
+    return '../../assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
+    this.select.emit(this.user.id);
     //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // }
