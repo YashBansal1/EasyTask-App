b7f970d545f9bc280a5c52b45d8a3fa026c0a985
Yash Bansal <Yash.Bansal@wissen.com>
2024-05-27 10:27:38 +0530

    display multiple user using Input decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,17 +10,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  imagePath = computed(
-    () => '../../assets/users/' + this.selectedUser().avatar
-  );
-  // get imagePath() {
-  //   return '../../assets/users/' + this.selectedUser().avatar;
-  // }
+  constructor() {
+    //this.avatar = DUMMY_USERS[0].avatar;
+  }
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // imagePath = computed(
+  //   () => '../../assets/users/' + this.selectedUser().avatar
+  // );
+  get imagePath() {
+    return '../../assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // }
   }
 }
